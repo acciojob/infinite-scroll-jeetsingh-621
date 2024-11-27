8e39d06dfe71d06cd2e395ac591877825e18912e
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-27 19:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+const listContainer = document.getElementById('infi-list');
+
+const initialItems = 10;
+const itemsToAdd = 2;
+
+function addListItems(count) {
+    for (let i = 1; i <= count; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `List Item ${listContainer.children.length + 1}`;
+        listContainer.appendChild(listItem);
+    }
+}
+
+addListItems(initialItems);
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        addListItems(itemsToAdd);
+    }
+});
 
